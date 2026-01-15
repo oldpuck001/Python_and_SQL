a67f0b007f6949d13f5b2078c3544ab07911bcd8
oldpuck001 <minimalistlei@gmail.com>
2026-01-15 23:48:16 +0800

    updata20260115

--- Python_basic/Chapter_01_GUI_Tkinter/tkinter_examples_1.py
+++ Python_basic/Chapter_01_GUI_Tkinter/tkinter_examples_1.py
@@ -2,7 +2,7 @@
 
 import tkinter as tk
 from tkinter import ttk
-from tkinter import messagebox, filedialog
+from tkinter import messagebox
 from tkinter.scrolledtext import ScrolledText
 
 def show_info():
